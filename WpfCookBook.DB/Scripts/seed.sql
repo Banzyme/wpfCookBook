48abcefff26a04464b159fffe7208f0b1a33b53d
N Nemakhavhani <ndamulelo@bbd.co.za>
2019-05-16 17:21:48 +0200

    Update seed script for new Dao's

--- WpfCookBook.DB/Scripts/seed.sql
+++ WpfCookBook.DB/Scripts/seed.sql
@@ -1,94 +1,94 @@
 --Insert seed data into modules table
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Basics(Getting started)');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Controls');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Layouts');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Data binding');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Data Templates');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Events');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Commands');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Resources');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Styles');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Media');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Graphics and Animations');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Misc');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'Advanced topics');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'References');
-insert WpfCourseModule(ModuleID, Name) values (NEWID(), 'FAQ');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Basics(Getting started)');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Controls');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Layouts');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Data binding');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Data Templates');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Events');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Commands');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Resources');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Styles');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Media');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Graphics and Animations');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Misc');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'Advanced topics');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'References');
+insert ModuleDao(ModuleID, Name) values (NEWID(), 'FAQ');
 
 --Insert seed to module sections - Tree view header
 -- 1.BASICS
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Quick intro', 1);
 
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'XAML fundamentals', 1);
 
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'What is WPF?', 1);
 
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Real life WPF examples', 1);
 
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Tutorial - Self service insuarence', 1);
 
 
 --2.CONTROLS - Child item on tree view
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Intro to WPF controls', 2);
 
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Text controls', 2);
- insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+ insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Buttons', 2);
- insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+ insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Checkboxes and Radio Buttons', 2);
- insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+ insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'List Views', 2);
 
-  insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+  insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Data Grid', 2);
 
-   insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+   insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Tree View', 2);
 
- insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+ insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Menus and Toolbars', 2);
- insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+ insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'HyperLinks', 2);
 
-  insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+  insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Misc', 2);
 
 
 --3. Layouts
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'WPF Layouts overview', 3);
 
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Grid Panel', 3);
 
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Stack Panel', 3);
 
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Dock Panel', 3);
 
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Canvas panel', 3);
 
-insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Wrap Panel', 3);
 
- insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+ insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Nested Layouts', 3);
 
- insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
+ insert ChapterDao(SectionID, Title, ModuleDao_ID)
  values (NEWID(), 'Tabs', 3);
 
 
  --Insert seed to module sections - Tab control
-  insert WpfCourseSectionItem(SectionItemID, Title,Subtitle, Content, WpfCourseSection_ID)
+  insert TopicDao(SectionItemID, Title,Subtitle, Content, ChapterDao_ID)
  values (NEWID(), 'Desktop applications', 
  'Really? Is this even necessary?',
 '<FlowDocument PagePadding="5,0,5,0" AllowDrop="True" xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation">
@@ -96,7 +96,7 @@ insert WpfCourseSection(SectionID, Title, WpfCourseModule_ID)
   2);
 
 
- insert WpfCourseSectionItem(SectionItemID, Title,Subtitle, Content, WpfCourseSection_ID)
+ insert TopicDao(SectionItemID, Title,Subtitle, Content, ChapterDao_ID)
  values (NEWID(), 'Terminology', 
  'Taming the jargon',
  '1. A control - Defines any element that the user interacts with in a WPF application',
